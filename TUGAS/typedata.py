fe1e879e7de9635a2a411642d8e52d8d91612165
yamkas <yamkasudirman@student.esaunggul.ac.id>
2022-10-10 11:31:07 +0700

    add bprog

---
+++ TUGAS/typedata.py
@@ -0,0 +1,59 @@
+# String
+a = "Hello"
+print (a)
+print (type(a))
+ 
+# Integer
+b = 12
+print (b)
+print (type(b))
+ 
+# Float
+c = 10.5
+print (c)
+print (type(c))
+ 
+# Tuple
+d = 20,5
+print (d)
+print (type(d))
+ 
+# Data COmplex
+e = 1j
+print (e)
+print (type(e))
+ 
+z = complex ('5-9j')
+print (z)
+ 
+# type data init
+f = ["a","b","c"]
+print (type(f))
+ 
+# type data set
+g = {"a","b","c"}
+print (type(g))
+ 
+# contoh
+list_f = [1,2,3]
+print ((list_f)[0])
+ 
+#contoh tuple
+tuple_f = 1,2,3
+print (tuple_f[0])
+ 
+# contoh set
+set_f = {1,2,3}
+print (set_f)
+ 
+#type data frozenset
+h = frozenset([1,2,3])
+print (type(b))
+ 
+#type data boolean
+i = True
+j = False
+print (i,j)
+print (type(i))
+print (type(j))
+
