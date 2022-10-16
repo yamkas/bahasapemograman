71034f759037dda72b1142b34dd05daed15f1e52
yamkas <yamkasudirman@student.esaunggul.ac.id>
2022-10-17 02:53:27 +0700

    add bprog

---
+++ pertemuan5/looping.py
@@ -0,0 +1,15 @@
+# looping with range
+for i in range(3):
+    print(i)
+
+for a in "pemograman":
+    if a =="p":
+        continue
+    if a =="g":
+        continue
+        # break 
+    print(a)  
+print("end")
+
+a = "pemograman"
+print(len(a))
---
+++ pertemuan5/readme.md
@@ -0,0 +1,5 @@
+# looping
+## looping with range
+## looping with break
+## looping with continue
+## nested loop
